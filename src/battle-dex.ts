ca53e10afa7adc2bd45aaad282cb0ed112df2d49
DieterReinert <dieter.reinert@outlook.com>
2021-08-15 17:11:01 +0200

    Update battle-dex.ts

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -695,8 +695,8 @@ const Dex = new class Dexes implements ModdedDex {
 			if (spriteData.gen >= 4 && miscData['frontf'] && options.gender === 'F') {
 				name += '-f';
 			}
-			// spriteData.url += dir + '/' + name + '.png';
-			spriteData.url = Config.hostURL + 'sprites/custom/' + name + '.png';
+			spriteData.url += dir + '/' + name + '.png';
+			//spriteData.url = Config.hostURL + 'sprites/custom/' + name + '.png';
 		}
 
 		if (!options.noScale) {
