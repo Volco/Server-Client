a29f7459bf1880cf22557aea155a3328b8d55040
Dieter Reinert <dieter.reinert@outlook.com>
2023-12-27 14:37:51 +0100

    fix

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1544,6 +1544,9 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 		let sketch = false;
 		let gen = '' + dex.gen;
 		let lsetTable = BattleTeambuilderTable;
+		console.log('format type, lsetTable["gen9sanctified"]:');
+		console.log(this.formatType);
+		console.log(lsetTable["gen9sanctified"])
 		if (this.formatType?.startsWith('sanctified')) lsetTable = lsetTable['gen9sanctified'];
 		if (this.formatType?.startsWith('bdsp')) lsetTable = lsetTable['gen8bdsp'];
 		if (this.formatType === 'letsgo') lsetTable = lsetTable['gen7letsgo'];
