f8c5c29eb100a1704aa9cf5a251f5b043309f139
Dieter Reinert <dieter.reinert@outlook.com>
2026-09-20 15:27:12 +0200

    Add Aevian formes to sprite names

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -697,6 +697,8 @@ const Dex = new class implements ModdedDex {
 				let animSuffix = allowAnim ? 'ani' : '';
 				let animExtension = allowAnim ? '.gif' : '.png';
 				animExtension = '.png';
+				// Aevian formes: 'drapionaevian' -> 'drapion-aevian' (keeps a trailing '-f' intact)
+				name = name.replace(/-?(aevian(?:east|west|fiery|icy|rocky)?)(-f)?$/, '-$1$2');
 				if (animationData[facing]) {
 					spriteData.w = animationData[facing].w;
 					spriteData.h = animationData[facing].h;
