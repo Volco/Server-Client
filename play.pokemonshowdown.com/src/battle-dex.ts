8fcd516290c455fbc277d2ed1c303fb0c3fa5242
Dieter Reinert <dieter.reinert@outlook.com>
2026-09-20 15:33:22 +0200

    debug

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -690,6 +690,7 @@ const Dex = new class implements ModdedDex {
 			if (spriteData.gen >= 4 && miscData['frontf'] && options.gender === 'F') {
 				name += '-f';
 			}
+			console.log(species.id);
 			if (species.num < 0 || species.id.endsWith('megag') || species.id.endsWith('aevian')) {
 				if (options.mod === 'digimon') {
 					animationData = BattleDigimonSprites[species.id];
